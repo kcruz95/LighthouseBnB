eececcea1c4a1c33ca77e3fec0f0b79e36d8847e
kcruz95 <kyle.cruz@hotmail.ca>
2021-07-21 23:09:58 -0700

    1_queries && 02_seeds

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -34,9 +34,9 @@ CREATE TABLE properties (
   number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
 
   country VARCHAR(255) NOT NULL,
-  street VARCHAR(255) NOT NULL,
-  city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
 
   active BOOLEAN NOT NULL DEFAULT TRUE
--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -11,10 +11,10 @@ VALUES (1, 1, '2018-09-11', '2018-09-26'),
 (4, 4, '2021-07-03', '2021-07-14');
 
 INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, province, city, street, post_code, active)
-VALUES (1, 'Posto 6', 'thumbnailPhoto', 'coverPhoto', 320, 1, 2, 3, 'Brazil', 'RJ', 'Rio de Janeiro', 'Rua Sa Ferreira'),
-(2, 'Baires', 'thumbnailPhoto', 'coverPhoto', 270, 0, 2, 2, 'Argentina', 'Buenos Aires', 'Buenos Aires', 'Calle Defensa'),
-(3, 'Village', 'thumbnailPhoto', 'coverPhoto', 420, 1, 3, 3, 'Canada', 'Quebec', 'Montreal', 'Cartier Street'),
-(4, 'Dachiao', 'thumbnailPhoto', 'coverPhoto', 115, 0, 2, 3, 'Taiwan', 'Taiwan', 'Taipei', 'Taishun Street'),
+VALUES (1, 'Posto 6', 'description', 'thumbnailPhoto', 'coverPhoto', 320, 1, 2, 3, 'Brazil', 'RJ', 'Rio de Janeiro', 'Rua Sa Ferreira'),
+(2, 'Baires', 'description', 'thumbnailPhoto', 'coverPhoto', 270, 0, 2, 2, 'Argentina', 'Buenos Aires', 'Buenos Aires', 'Calle Defensa'),
+(3, 'Village', 'description', 'thumbnailPhoto', 'coverPhoto', 420, 1, 3, 3, 'Canada', 'Quebec', 'Montreal', 'Cartier Street'),
+(4, 'Dachiao', 'description', 'thumbnailPhoto', 'coverPhoto', 115, 0, 2, 3, 'Taiwan', 'Taiwan', 'Taipei', 'Taishun Street'),
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id,rating, message)
 VALUES (1, 1, 1, 3,'msg1'),
